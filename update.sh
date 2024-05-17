249d5af0f0a9b088ab77f8cabeaca76a4204b19a
Hannes <Hannes.o@t-online.de>
2024-05-17 09:39:01 +0200

    add File to update all nessesary python packages

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/hannes/Desktop/OctaControl/workspace/
+
+source /home/hannes/Desktop/OctaControl/workspace/myenv/bin/activate
+
+
+pip install Flask
+pip install Flask[async]
+pip install pydbus
+pip install pillow
+pip install obd
+sudo apt-get instal python3-alsaaudio
+pip install pyserial
+pip install gevent
+pip install flask-socketio eventlet pydbus pillow
